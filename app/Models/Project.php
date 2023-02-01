45a2c3d9c680f1616d7d9350ee9fb57ee98069e5
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-02-01 23:37:43 +0100

    Crea metodo getImageUrlAttribute per avere url img

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,6 +10,13 @@ class Project extends Model
     use HasFactory;
 
     protected $guarded = ['slug'];
+    protected $appends = ['image_url'];
+
+    // metodo per aggiungere una proprietà al model se non abbiamo una colonna a database con l'obiettivo di restituire l'url completo dell'immagine
+    protected function getImageUrlAttribute()
+    {
+        return $this->picture ? asset("storage/$this->picture") : 'https://via.placeholder.com/400x200';
+    }
 
     public function type()
     {
